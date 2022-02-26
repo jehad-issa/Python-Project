21dd5b107dd77c4c483ce3f11fd257f948894c0d
oday qsrawi <odayqsrawi@gmail.com>
2022-02-26 09:54:35 +0200

    trader_fron_end

--- greengrocer/greengrocer_app/urls.py
+++ greengrocer/greengrocer_app/urls.py
@@ -4,6 +4,8 @@ from . import views	# the . indicates that the views file can be found in the sa
 urlpatterns = [
     path('farmer', views.farmer),
     path('farmer/add_crop',views.add_crop),
-    path('farmer/edit_crop',views.edit_crop),
-    path('sales',views.sales),
+    path('farmer/edit_crop/1',views.edit_crop),
+    path('trader', views.trader),
+    path('trader/buy', views.trader_buy_crop),
+    path('trader/purchases', views.purchases),
 ]
--- greengrocer/login_app/models.py
+++ greengrocer/login_app/models.py
@@ -1,3 +1,37 @@
+from tkinter import CASCADE
+from urllib import request
 from django.db import models
 
-# Create your models here.
+class Farmer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    phone_number=models.IntegerField()
+    city = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Trader(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    phone_number=models.IntegerField()
+    city = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Crop(models.Model):
+    crop_name=models.CharField(max_length=255)
+    quantity=models.IntegerField()
+    quality=models.CharField(max_length=3)
+    price=models.IntegerField()
+    desc=models.TextField()
+    farmer=models.ForeignKey(Farmer,related_name='crops',on_delete=CASCADE)
+    traders=models.ManyToManyField(Trader,through='Sale')
+
+class Sale(models.Model):
+    crop=models.ForeignKey(Crop,on_delete=CASCADE)
+    trader=models.ForeignKey(Trader,on_delete=CASCADE)
+    quantity=models.IntegerField()
